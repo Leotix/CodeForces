67f35aad205282d452c261f878eb43f981cb1ac5
Leotix <47683040+Leotix@users.noreply.github.com>
2022-04-13 17:16:30 +0200

    Added solution to problem 977A in C

---
+++ 977A-WrongSubtraction/977a-wrongSubtraction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(){
+	int n,k;
+	scanf("%d %d", &n, &k);
+	while(k > 0){
+		if (n % 10 != 0){
+			n -= 1;
+		}
+		else{
+			n = n / 10;
+		}
+		k--;
+	}
+	printf("%d",n);
+}
