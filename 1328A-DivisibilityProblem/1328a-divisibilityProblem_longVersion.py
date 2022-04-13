9f4d65c78ec265d60bdd6e64ec3dc8558582bd4d
Leotix <47683040+Leotix@users.noreply.github.com>
2022-04-13 18:10:27 +0200

    Update 1328a-divisibilityProblem_longVersion.py

--- 1328A-DivisibilityProblem/1328a-divisibilityProblem_longVersion.py
+++ 1328A-DivisibilityProblem/1328a-divisibilityProblem_longVersion.py
@@ -1,9 +1,10 @@
-lst = []
-for _ in range(int(input())):
-    p = [int(x) for x in input().split()]
-    if p[0] % p[1] == 0:
-        lst.append(0)
-    else:
-        lst.append(p[1] - p[0] % p[1])
-for num in lst:
-    print(num)
+if __name__ == "__main__":
+    lst = []
+    for _ in range(int(input())):
+        p = [int(x) for x in input().split()]
+        if p[0] % p[1] == 0:
+            lst.append(0)
+        else:
+            lst.append(p[1] - p[0] % p[1])
+    for num in lst:
+        print(num)
