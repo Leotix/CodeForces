cb5554050bad4565fb2e037499c67fbfd6378bca
Leotix <47683040+Leotix@users.noreply.github.com>
2022-04-13 18:09:00 +0200

    Added solution to problem 1328A in Python
    Long version of solution to problem 1328A

---
+++ 1328A-DivisibilityProblem/1328a-divisibilityProblem_longVersion.py
@@ -0,0 +1,9 @@
+lst = []
+for _ in range(int(input())):
+    p = [int(x) for x in input().split()]
+    if p[0] % p[1] == 0:
+        lst.append(0)
+    else:
+        lst.append(p[1] - p[0] % p[1])
+for num in lst:
+    print(num)
